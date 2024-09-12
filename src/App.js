5b7982dfe340a59123089eeddaa370b5270e0ff3
Jaswanth-Kollipara <kolliparajaswanth5@gmail.com>
2024-09-12 20:16:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {Route, Switch} from 'react-router-dom'
-import Page1 from './components/Page1'
+import Home from './components/Home'
 import Page2 from './components/Page2'
 import Page3 from './components/Page3'
 import Page4 from './components/Page4'
@@ -8,7 +8,7 @@ import './App.css'
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={Page1} />
+    <Route exact path="/" component={Home} />
     <Route exact path="/page2" component={Page2} />
     <Route exact path="/page3" component={Page3} />
     <Route exact path="/page4" component={Page4} />
